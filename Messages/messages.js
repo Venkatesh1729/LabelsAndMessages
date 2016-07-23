645060c6d946c7e14f8b7077dd77deae650121c0
Venkatesh <venki.172011@gmail.com>
2016-07-23 22:58:03 +0530

    Messages Folder Updated.

---
+++ Messages/messages.js
@@ -0,0 +1,12 @@
+var Messages={
+MESSAGE1 : "Message 1",
+MESSAGE2 : "Message 2",
+MESSAGE3 : "Message 3",
+MESSAGE4 : "Message 4",
+MESSAGE5 : "Message 5",
+MESSAGE6 : "Message 6",
+MESSAGE7 : "Message 7",
+MESSAGE8 : "Message 8",
+MESSAGE9 : "Message 9",
+MESSAGE10 : "Message 10"
+};
