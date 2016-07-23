3883b824c860104fd7447571d2785d18f2b18f96
Venkatesh <venki.172011@gmail.com>
2016-07-23 22:42:14 +0530

    Labels Folder Update.

---
+++ Labels/labels.js
@@ -0,0 +1,12 @@
+var Labels={
+LABEL1 : "Label 1",
+LABEL2 : "Label 2",
+LABEL3 : "Label 3",
+LABEL4 : "Label 4",
+LABEL5 : "Label 5",
+LABEL6 : "Label 6",
+LABEL7 : "Label 7",
+LABEL8 : "Label 8",
+LABEL9 : "Label 9",
+LABEL10 : "Label 10"
+};
